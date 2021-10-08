8c92b40255908438c4186bb99e5549e4a41c1a66
Ranjana Kambhammettu <ranjanak207@gmail.com>
2021-10-08 22:13:26 +0400

    Update checking

--- check.js
+++ check.js
@@ -10,5 +10,7 @@ function checkAll(){
     var emailCheck=/[\S]/.test(email) + /@/.test(email) + /\./.test(email) ;
     var contactCheck= /^\d+$/.test(contact);
     var genreCheck= /[\w]/.test(genre);
-    alert(genreCheck);
+    var checkAll = nameCheck * dobCheck * emailCheck * contactCheck * genreCheck;
+    if (checkAll===0)
+    alert("Make sure you have entered the fields correctly!");
  }
