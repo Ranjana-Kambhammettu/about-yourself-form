c23a1ad4c8821df546ed4ac1347680929065a777
Ranjana Kambhammettu <ranjanak207@gmail.com>
2021-10-08 14:01:13 +0400

    Added a few checks

--- check.js
+++ check.js
@@ -4,6 +4,11 @@ function checkAll(){
     var email= document.getElementById("email").value;
     var contact= document.getElementById("contact").value;
     var genre= document.getElementById("genre").value;
-    var nameCheck;
-    alert(nameCheck);
+    var nameCheck= /[\w]/.test(name);
+    let date=/\d{1,2}-\d{1,2}-\d{4}/;
+    var dobCheck= date.test(name);
+    var emailCheck=/[\S]/.test(email) + /@/.test(email) + /\./.test(email) ;
+    var contactCheck= /^\d+$/.test(contact);
+    var genreCheck= /[\w]/.test(genre);
+    alert(genreCheck);
  }
