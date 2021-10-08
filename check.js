18fe7d94de3b1d9405b93653bcaaef62105a3741
Ranjana Kambhammettu <ranjanak207@gmail.com>
2021-10-08 13:17:01 +0400

    Add check,js

---
+++ check.js
@@ -0,0 +1,9 @@
+function checkAll(){
+    var name= document.getElementById("name").value;
+    var dob= document.getElementById("DOB").value;
+    var email= document.getElementById("email").value;
+    var contact= document.getElementById("contact").value;
+    var genre= document.getElementById("genre").value;
+    var nameCheck;
+    alert(nameCheck);
+ }
