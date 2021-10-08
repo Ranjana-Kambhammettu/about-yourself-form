2162396e35c379b1f52033a8ab1c0755093f28da
Ranjana Kambhammettu <ranjanak207@gmail.com>
2021-10-08 22:57:07 +0400

    Fixed check and footer

--- check.js
+++ check.js
@@ -6,7 +6,7 @@ function checkAll(){
     var genre= document.getElementById("genre").value;
     var nameCheck= /[\w]/.test(name);
     let date=/\d{1,2}-\d{1,2}-\d{4}/;
-    var dobCheck= date.test(name);
+    var dobCheck= date.test(dob);
     var emailCheck=/[\S]/.test(email) + /@/.test(email) + /\./.test(email) ;
     var contactCheck= /^\d+$/.test(contact);
     var genreCheck= /[\w]/.test(genre);
